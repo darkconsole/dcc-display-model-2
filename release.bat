e419cf5c4a0260facfc421e0cfa898df05a4515c
darkconsole <darkcee.legit@gmail.com>
2016-11-22 11:03:51 -0600

    a friendly reminder to myself

--- release.bat
+++ release.bat
@@ -1,1 +1,3 @@
-zip display-model-v%~1.zip -r *.esp notes-*.txt interface meshes scripts seq textures
+@echo off
+echo DID YOU INCREMEMENT THE VERSION NUMBER IN THE MAIN SCRIPT ASSHOLE?
+zip display-model-v%~1.zip -q -r *.esp notes-*.txt interface meshes scripts seq textures
